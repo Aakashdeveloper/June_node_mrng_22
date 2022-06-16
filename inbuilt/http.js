5dcc8ee46aeba529ef95e2946db44f3c42fcb458
aakashdeveloper <ahanda205@gmail.com>
2022-06-16 08:41:50 +0530

    staticapi

--- inbuilt/http.js
+++ inbuilt/http.js
@@ -4,8 +4,8 @@ let http = require('http');
 // res > what server will respond
 
 let server = http.createServer(function(req, res){
-    res.write('<h1>Hiii From Node Server</h1>');
+    res.write('<h1 style="color:red">Hiii From NodeJs Server App </h1>');
     res.end();
 })
 
-server .listen(9700)
+server .listen(1543)
