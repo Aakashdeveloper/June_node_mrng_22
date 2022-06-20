d0fade679d1efebadb7274004b3c644a5b9eb567
aakashdeveloper <ahanda205@gmail.com>
2022-06-20 08:56:06 +0530

    expressrouter

--- fullstack/app.js
+++ fullstack/app.js
@@ -1,16 +1,18 @@
 let express = require('express');
 let app = express();
-let port = 8790;
+let dotenv = require('dotenv');
+dotenv.config()
+let port = process.env.PORT || 8790;
+let categoryRouter = require('./src/router/categoryRouter')
+let productRouter = require('./src/router/productRouter.js');
 
 //routes
 app.get('/',function(req,res){
     res.send("Hii From Default Route")
 })
 
-app.get('/location',function(req,res){
-    res.send('This is location route')
-})
-
+app.use('/category', categoryRouter);
+app.use('/products', productRouter);
 
 // created server
 app.listen(port,function(err){
